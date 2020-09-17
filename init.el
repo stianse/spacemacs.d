7d086ee19f66784c087eab5c5adf781d9684463f
Stian Selnes <stian.selnes@gmail.com>
2020-09-17 17:32:42 +0200

    Add backups per-session and per-save

--- init.el
+++ init.el
@@ -562,4 +562,35 @@ before packages are loaded."
     :ensure t
     :config (spacemacs|hide-lighter dtrt-indent-mode)
     :hook (prog-mode . dtrt-indent-mode))
+  ;; (setq
+  ;;  evil-want-fine-undo nil)
+
+  ;; Backups
+  ;; See https://github.com/syl20bnr/spacemacs/issues/8947
+  (setq make-backup-files t)
+  (setq version-control t     ;; Use version numbers for backups.
+        kept-new-versions 10  ;; Number of newest versions to keep.
+        kept-old-versions 0   ;; Number of oldest versions to keep.
+        delete-old-versions t ;; Don't ask to delete excess backup versions.
+        backup-by-copying t)  ;; Copy all files, don't rename them.
+  (setq vc-make-backup-files t)
+  ;; Default and per-save backups go here:
+  (setq backup-directory-alist '(("" . "/home/stianse/.emacs.d/.cache/backups/per-save")))
+
+  (defun force-backup-of-buffer ()
+    (message "force backup")
+    ;; Make a special "per session" backup at the first save of each
+    ;; emacs session.
+    (when (not buffer-backed-up)
+      (message "no buffer-backed-up")
+      ;; Override the default parameters for per-session backups.
+      (let ((backup-directory-alist '(("" . "/home/stianse/.emacs.d/.cache/backups/per-session")))
+            (kept-new-versions 3))
+        (backup-buffer)))
+    ;; Make a "per save" backup on each save.  The first save results in
+    ;; both a per-session and a per-save backup, to keep the numbering
+    ;; of per-save backups consistent.
+    (let ((buffer-backed-up nil))
+      (backup-buffer)))
+  (add-hook 'before-save-hook  'force-backup-of-buffer)
 )
