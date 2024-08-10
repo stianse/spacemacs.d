903960825213b54eefbd93c5921dcd42bcc6e846
Stian Selnes <stian.selnes@gmail.com>
2024-08-10 19:41:34 +0200

    git: Add git-link package
    git-link is very useful to quickly open commits and files on gitlab

--- init.el
+++ init.el
@@ -70,7 +70,7 @@ This function should only modify configuration layer settings."
    ;; `dotspacemacs/user-config'. To use a local version of a package, use the
    ;; `:location' property: '(your-package :location "~/path/to/your-package/")
    ;; Also include the dependencies as they will not be resolved automatically.
-   dotspacemacs-additional-packages '(dtrt-indent)
+   dotspacemacs-additional-packages '(dtrt-indent git-link)
 
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
