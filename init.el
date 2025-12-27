6de3eacdac4fb97dc943ac3e500b961824381fba
Stian Selnes <stian.selnes@gmail.com>
2025-12-27 16:16:54 +0100

    Add rust, lua and toml layers

--- init.el
+++ init.el
@@ -32,7 +32,10 @@ This function should only modify configuration layer settings."
 
    ;; List of configuration layers to load.
    dotspacemacs-configuration-layers
-   '(html
+   '(rust
+     lua
+     toml
+     html
      javascript
      python
      ;; ----------------------------------------------------------------
