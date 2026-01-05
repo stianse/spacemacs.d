c6915dbaadebd43f9be93b20aa3aea053a030539
Stian Selnes <stian.selnes@gmail.com>
2026-01-05 20:22:26 +0100

    org-agenda: Include private calendar, non-work custom command etc
    Several changes:
    * Include private calendar file
    * Custom command to view non-work projects
    * Define todo-keywords in SETUPFILE in order to not affect org-mode in general
    * Change Inbox command to "catch all" in case keywords is missing
    * Add custom command to list all projects

--- user-config-org-agenda.el
+++ user-config-org-agenda.el
@@ -1,44 +1,73 @@
 (setq org-agenda-files '("~/Dropbox/gtd/inbox.org"
-                         "~/Dropbox/gtd/gtd.org"
-                         "~/Dropbox/gtd/tickler.org"
-                         "~/Dropbox/gtd/gcal.org"))
+                         "~/Dropbox/gtd/gtd.org"))
 
 (setq org-capture-templates '(("t" "Todo [inbox]" entry
                                (file+headline "~/Dropbox/gtd/inbox.org" "Tasks")
-                               "* TODO %i%?")
-                              ("T" "Tickler" entry
-                               (file+headline "~/Dropbox/gtd/tickler.org" "Tickler")
-                               "* %i%? \n %U")))
+                               "* TODO %i%?")))
 
 (setq org-refile-targets '(("~/Dropbox/gtd/gtd.org" :maxlevel . 3)
-                           ("~/Dropbox/gtd/someday.org" :level . 1)
-                           ("~/Dropbox/gtd/tickler.org" :maxlevel . 2)))
-
-;; Add ! to record time of state changes
-(setq org-todo-keywords '((sequence "TODO(t!)" "WAIT(w!)" "|" "DONE(d!)" "CANCELED(c!)")))
+                           ("~/Dropbox/gtd/someday.org" :level . 1)))
 
 ;; Fetch tags completation from all agenda files
 (setq org-complete-tags-always-offer-all-agenda-tags t)
 
 (setq org-agenda-custom-commands
-      '(("i" "Inbox" todo ""
+      '(("i" "Inbox" tags "-DONE- | -CANCELED-" ;; trick for a special catch all (with and without a todo state)
          ((org-agenda-files '("~/Dropbox/gtd/inbox.org"))
           (org-agenda-overriding-header "Inbox items")))
         ("w" "Work"
          ((agenda ""
-                  ((org-agenda-span 2)
-                   (org-agenda-use-time-grid nil)))
-          (tags-todo "@work"
-                     ((org-agenda-overriding-header "Work")
+                  ((org-agenda-files '("~/Dropbox/gtd/gcal-work.org"))
+                   (org-agenda-span 2)
+                   (org-agenda-use-time-grid nil)
+                   (org-agenda-prefix-format "  %?-12t% s")
+                   (org-agenda-remove-tags t)))
+          (tags-todo "+project+@work"
+                     ((org-agenda-overriding-header "Next for projects")
                       (org-agenda-prefix-format "%-32:(my/org-agenda-format-parent 30)")
                       (org-agenda-todo-keyword-format "%-4s")
+                      (org-agenda-hide-tags-regexp (regexp-opt '("project" "@work")))
                       (org-agenda-skip-function #'my/org-agenda-skip-all-siblings-but-first)
                       (org-agenda-sorting-strategy '(user-defined-down))
-                      (org-agenda-cmp-user-defined #'my/org-agenda-cmp-parent-priority)))))
+                      (org-agenda-cmp-user-defined #'my/org-agenda-cmp-parent-priority)))
+          (tags-todo "-project+@work"
+                     ((org-agenda-overriding-header "Next single actions")
+                      (org-agenda-todo-keyword-format "%-4s")
+                      (org-agenda-hide-tags-regexp (regexp-opt '("@work")))
+                      (org-agenda-sorting-strategy '(user-defined-down))
+                      (org-agenda-cmp-user-defined #'my/org-agenda-cmp-parent-priority)))
+          ))
         ("o" "Office only" tags-todo "@office"
          ((org-agenda-overriding-header "Office")
           (org-agenda-prefix-format "%-32:(my/org-agenda-format-parent 30)")
           (org-agenda-todo-keyword-format "%-4s")))
+        ("W" "Non-work"
+         ((agenda ""
+                  ((org-agenda-files '("~/Dropbox/gtd/gcal-private.org"))
+                   (org-agenda-span 2)
+                   (org-agenda-use-time-grid nil)
+                   (org-agenda-prefix-format "  %?-12t% s")
+                   (org-agenda-remove-tags t)))
+          (tags-todo "+project-@work"
+                     ((org-agenda-overriding-header "Next for projects")
+                      (org-agenda-prefix-format "%-32:(my/org-agenda-format-parent 30)")
+                      (org-agenda-todo-keyword-format "%-4s")
+                      (org-agenda-hide-tags-regexp (regexp-opt '("project" "@home")))
+                      (org-agenda-skip-function #'my/org-agenda-skip-all-siblings-but-first)
+                      (org-agenda-sorting-strategy '(user-defined-down))
+                      (org-agenda-cmp-user-defined #'my/org-agenda-cmp-parent-priority)))
+          (tags-todo "-project-@work"
+                     ((org-agenda-overriding-header "Next single actions")
+                      (org-agenda-todo-keyword-format "%-4s")
+                      (org-agenda-hide-tags-regexp (regexp-opt '("@home")))
+                      (org-agenda-sorting-strategy '(user-defined-down))
+                      (org-agenda-cmp-user-defined #'my/org-agenda-cmp-parent-priority)))
+          ))
+        ("p" "Projects"
+         ((tags "project"
+                ((org-agenda-overriding-header "All projects")
+                 (org-agenda-hide-tags-regexp (regexp-opt '("project")))
+                 (org-use-tag-inheritance nil)))))
         ("u" "Untagged tasks" tags-todo "-{.*}")
         ))
 
